c1fa6a87b02f8716c5327c6f796a9558f3f655ef
Troels H. Jensen <lord_ni@hotmail.com>
2018-05-17 14:04:38 +0200

    Empty file for NodeJS WS Access

---
+++ MOM/WebServiceControllers/AttendanceLogWS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// TODO: Connect to NodeJSWS
+// TODO: Create methods to correspond with WS endpoints
+namespace MOM.WebServiceControllers
+{
+    public class AttendanceLogWS
+    {
+
+    }
+}
