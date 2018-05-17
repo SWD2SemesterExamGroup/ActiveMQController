a45724e1cbccf901b81a1dca8cfeba2ca2a7dd8d
Troels H. Jensen <lord_ni@hotmail.com>
2018-05-17 14:03:29 +0200

    Setup Student Con/Pro

---
+++ MOM/ActiveMQ/Student/StudentConsumer.cs
@@ -0,0 +1,57 @@
+﻿namespace MOM.ActiveMQ.Student
+{
+    using Apache.NMS;
+    using Apache.NMS.Util;
+    using System;
+    using System.Diagnostics;
+
+    public class StudentConsumer : Configuration
+    {
+        private IDestination studentDestiantion;
+        private const String PATH_KEY_STUDENT = "queue://student-key";
+
+        // Receiving messages from student trying out a key
+        public void studentKey()
+        {
+            using (IConnection connection = factory.CreateConnection(USER, PASSWORD))
+            using (ISession session = connection.CreateSession())
+            {
+                studentDestiantion = SessionUtil.GetDestination(session, PATH_KEY_STUDENT);
+                Console.WriteLine("Using destination: " + studentDestiantion);
+
+                // Create a consumer
+                using (IMessageConsumer consumer = session.CreateConsumer(studentDestiantion))
+                {
+                    connection.Start();
+
+                    /*
+                     Console.WriteLine();
+                     Debug.WriteLine();
+                     */
+
+                    while (true)
+                    {
+                        ITextMessage message = consumer.Receive() as ITextMessage;
+                        if (message == null)
+                        {
+                            Debug.WriteLine("No message received!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Message Received!!: " + message.Text);
+                            Debug.WriteLine("Message Received!!: \n" + message.Text);
+
+                            // TODO: Filter and deligate messages to diff webservices
+
+                            /*
+                             * try key at php ws get all ids and a success message response
+                             * call attendance log WS to post data
+                             * Return success response to student client queue student-key
+                             */
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MOM/ActiveMQ/Student/StudentProducer.cs
@@ -0,0 +1,44 @@
+﻿namespace MOM.ActiveMQ.Student
+{
+    using Apache.NMS;
+    using Apache.NMS.Util;
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Student Producer sends messages to the MQ Broker for the student functionality
+    /// </summary>
+    /// <seealso cref="MOM.ActiveMQ.Configuration" />
+    public class StudentProducer : Configuration
+    {
+        private IDestination Destination;
+        private const String PATH_QUEUE = "queue://student-key";
+
+        public void ResponseToKeyAttendance(String message)
+        {
+            using (IConnection connection = factory.CreateConnection(USER, PASSWORD))
+            using (ISession session = connection.CreateSession())
+            {
+                Destination = SessionUtil.GetDestination(session, PATH_QUEUE);
+                Console.WriteLine("Using destination: " + Destination);
+
+                // Create a consumer
+                using (IMessageProducer producer = session.CreateProducer(Destination))
+                {
+                    // Start the connection so that messages will be processed.
+                    connection.Start();
+
+                    producer.DeliveryMode = MsgDeliveryMode.Persistent;
+
+                    ITextMessage request = session.CreateTextMessage(message);
+
+                    Console.WriteLine(request.Text);
+                    Debug.WriteLine(request.Text);
+
+                    producer.Send(request.Text);
+                    Console.WriteLine("Message Send to ActiveMQ Queue");
+                }
+            }
+        }
+    }
+}
