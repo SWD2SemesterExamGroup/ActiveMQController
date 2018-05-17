8a02ed0d01f36617ae64fbca3c4c602053f722dc
Troels H. Jensen <lord_ni@hotmail.com>
2018-05-17 17:46:18 +0200

    Finalized for UC#Use generated Keys

--- MOM/WebServiceControllers/AttendanceLogWS.cs
+++ MOM/WebServiceControllers/AttendanceLogWS.cs
@@ -1,14 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-// TODO: Connect to NodeJSWS
-// TODO: Create methods to correspond with WS endpoints
+
 namespace MOM.WebServiceControllers
 {
     public class AttendanceLogWS
     {
+        // Connection Variables
+        private const String BASE_PATH = "http://localhost";
+        private const String BASE_PORT = ":8000";
+        private const String BASE_POST = "/insertcourseregistration";
+        private String destination;
 
+        public string addToAttendanceLog(JObject data)
+        {
+            destination = BASE_PATH + BASE_PORT + BASE_POST;
+            WebClient client = new WebClient();
+            Debug.WriteLine("Destination: " + destination);
+            Debug.WriteLine("Data: " + data.ToString());
+
+            NameValueCollection nvCollection = new NameValueCollection();
+            nvCollection.Add("StudentID", (String)data["studentID"]);
+            nvCollection.Add("CourseID", (String)data["courseid"]);
+            nvCollection.Add("successAttempt", (String)data["success"]);
+            // Add params to body
+
+            // Send data and get response
+            byte[] response = client.UploadValues(destination, "POST", nvCollection);
+
+            // return response in string
+            return Encoding.ASCII.GetString(response);
+        }
     }
 }
