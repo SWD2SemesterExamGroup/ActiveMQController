b1928e97427cabf620bc6bf62ebc1952b5f2658a
Troels H. Jensen <lord_ni@hotmail.com>
2018-05-17 14:02:46 +0200

    Updated to responde with Client

--- MOM/ActiveMQ/NMSProducer.cs
+++ MOM/ActiveMQ/NMSProducer.cs
@@ -2,51 +2,22 @@
 {
     using Apache.NMS;
     using Apache.NMS.Util;
-    using MOM.KEA_Organization;
     using Newtonsoft.Json.Linq;
     using System;
     using System.Diagnostics;
-    using System.Web.Services.Protocols;
 
+    /// <summary>
+    /// A common producer used for the teacher client app
+    /// </summary>
+    /// <seealso cref="MOM.ActiveMQ.Configuration" />
     public class NMSProducer : Configuration
     {
-        private IDestination destination;
-        private const String PATH_QUEUE = "queue://jsa-queue";
-
-        public void start(JObject jWS, ISession session, IConnection connection)
-        {
-            using (session = connection.CreateSession())
-            {
-                destination = SessionUtil.GetDestination(session, PATH_QUEUE);
-                Console.WriteLine("Using destination: " + destination);
-
-                // Create a consumer
-                using (IMessageProducer producer = session.CreateProducer(destination))
-                {
-                    // Start the connection so that messages will be processed.
-                    connection.Start();
-                    
-                    producer.DeliveryMode = MsgDeliveryMode.Persistent;
-
-                    // Send a message
-                    //IMessageProducer prod = session.CreateProducer();
-                    //prod.DisableMessageID = false;
-                    //prod.Send(destination, prod.CreateTextMessage(jWS.ToString()));
-
-                    ITextMessage request = session.CreateTextMessage(jWS.ToString());
-                    
-                    Console.WriteLine(request.Text);
-                    Debug.WriteLine(request.Text);
-
-                    producer.Send(request);
-                    Console.WriteLine("Message Send to ActiveMQ Queue");
-                }
-            }
-        }
-
+        private IDestination destination, destinationKeySuccess;
+        private const String PATH_QUEUE = "queue://jsa-queue", PATH_KEY_SUCCESS = "queue://course-key-response";
+        
         public void start(string text)
         {
-            using (IConnection connection = factory.CreateConnection(Configuration.USER, Configuration.PASSWORD))
+            using (IConnection connection = factory.CreateConnection(USER, PASSWORD))
             using (ISession session = connection.CreateSession())
             {
                 destination = SessionUtil.GetDestination(session, PATH_QUEUE);
@@ -60,11 +31,6 @@
 
                     producer.DeliveryMode = MsgDeliveryMode.Persistent;
 
-                    // Send a message
-                    //IMessageProducer prod = session.CreateProducer();
-                    //prod.DisableMessageID = false;
-                    //prod.Send(destination, prod.CreateTextMessage(jWS.ToString()));
-
                     ITextMessage request = session.CreateTextMessage(text);
 
                     Console.WriteLine(request.Text);
@@ -72,6 +38,33 @@
 
                     producer.Send(request.Text);
                     Console.WriteLine("Message Send to ActiveMQ Queue");
+                }
+            }
+        }
+
+        public void keyResponse(string text)
+        {
+            using (IConnection connection = factory.CreateConnection(USER, PASSWORD))
+            using (ISession session = connection.CreateSession())
+            {
+                destinationKeySuccess = SessionUtil.GetDestination(session, PATH_KEY_SUCCESS);
+                Console.WriteLine("Using destination: " + destinationKeySuccess);
+
+                // Create producer
+                using (IMessageProducer producer = session.CreateProducer(destinationKeySuccess))
+                {
+                    // Start connection
+                    connection.Start();
+
+                    producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
+
+                    ITextMessage request = session.CreateTextMessage(text);
+
+                    Console.WriteLine(request.Text);
+                    Debug.WriteLine(request.Text);
+
+                    producer.Send(request.Text);
+                    Console.WriteLine("Message Send!!\nMessage: " + text + "\nDestination: " + destinationKeySuccess);
                 }
             }
         }
